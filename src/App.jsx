2e2f38a89bc6aef7111022a4581fe62eab77e23f
Mijael <mijael691@gmail.com>
2023-06-17 19:34:48 -0500

    Add Navbar and theme switch button

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,45 @@
 import React from 'react'
-// import { Button } from '@mui/material'
-import Button from "@mui/material/Button";
+import { Navbar, PersistentDrawer } from './components'
+import CssBaseline from '@mui/material/CssBaseline';
+import ThemeProvider from '@mui/material/styles/ThemeProvider';
+import createTheme from '@mui/material/styles/createTheme';
+import { Box } from '@mui/material';
 
+
+export const ColorModeContext = React.createContext({ toggleColorMode: () => {} });
 
 export default function App() {
+  const [mode, setMode] = React.useState('dark');
+  const colorMode = React.useMemo(
+    () => ({
+      toggleColorMode: () => {
+        setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
+      },
+    }),
+    [],
+  );
+
+  const theme = React.useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode,
+        },
+      }),
+    [mode],
+  );
+
   return (
     <>
-    <h1>App</h1>
-    <Button variant ="contained">Mi boton</Button>
+      <CssBaseline/>
+      <ColorModeContext.Provider value={colorMode}>
+        <ThemeProvider theme={theme}>
+          <Box sx={{ bgcolor: '#cfe8fc', height: '100vh' }}>
+            <Navbar />
+          {/* <PersistentDrawer /> */}
+          </Box>
+        </ThemeProvider>
+      </ColorModeContext.Provider>
     </>
   )
 }
